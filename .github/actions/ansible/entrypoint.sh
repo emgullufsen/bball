2f598c14474ec92280638c9c461319c4050cb25a
rickygee <ericgullufsen@gmail.com>
2021-04-15 00:30:31 -0800

    ansible eleven

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,10 +4,10 @@ mkdir ~/.ssh
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DKPASS" \
---output $HOME/secrets/appery_key2 ~/appery_key2.gpg
+--output $HOME/secrets/appery_key2 appery_key2.gpg
 eval "$(ssh-agent -s)"
 chmod 600 $HOME/secrets/appery_key2
 echo -e "Host rickysquid.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add $HOME/secrets/appery_key2
 ssh -i $HOME/secrets/appery_key2 apper@rickysquid.org pwd
-ansible-playbook -i ~/e_inventory.yml ~/e_playbook.yml
+ansible-playbook -i e_inventory.yml e_playbook.yml
