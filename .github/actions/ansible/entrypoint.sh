556334b944c4fddc977a29de7b128050ad0426a4
rickygee <ericgullufsen@gmail.com>
2021-04-14 23:36:30 -0800

    ansible eight

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,5 +1,6 @@
 # Decrypt the file
 mkdir $HOME/secrets
+mkdir ~/.ssh
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DKPASS" \
